3ef814d7112ba88657cf97ed86e992d5bad10400
Abhilash <abhilash@heidelsoft-technologies.de>
2018-12-31 18:31:12 +0530

    Added Globel variabl
    Added Globel variable for logged in user_id for time being till we implement JWT

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers;
 use App\Model\Like;
 use Illuminate\Http\Request;
 use App\Model\Reply;
+use Config;
 
 class LikeController extends Controller
 {
     public function Liked(Reply $reply) {
         $reply->like()->create([
             // 'user_id'   => auth->id(),
-            'user_id'   => '1'
+            'user_id'   => Config::get('app.app_user_id')
         ]);
     }
    
-    public function Unliked(Reply $reply) {
+    public function Unliked(Reply $reply) {      
         //$reply->like()->where(['user_id',auth()->id()])->first()->delete();
-        $reply->like()->where('user_id', '1')->first()->delete();
+        $reply->like()->where('user_id', Config::get('app.app_user_id'))->first()->delete();
     }
 }
