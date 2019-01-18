2902f947b141aa7b240a5ec4977fa9ddf9c3d5cd
Abhilash <abhilash@heidelsoft-technologies.de>
2019-01-18 16:52:50 +0530

    Added JWT

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,14 @@ Route::apiResource('/questions/{question}/reply','ReplyController');
 
 Route::post('/like/{reply}', 'LikeController@Liked');
 Route::delete('/like/{reply}', 'LikeController@Unliked');
+
+Route::group([
+    // 'middleware' => 'api',
+    'prefix' => 'auth'
+], function () { //$router
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+});
